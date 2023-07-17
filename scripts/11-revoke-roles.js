85ec6806b084c5f2141e3888cd99e94d9056b8da
MattWong-ca <matthew.yt.wong@gmail.com>
2023-07-17 14:35:23 -0400

    Revoke admin + minter role

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,22 @@
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    const token = await sdk.getContract("0xF5AD13e83eB41183A408e37F4aC41eB943e0D0f6", "token");
+    // Log the current roles.
+    const allRoles = await token.roles.getAll();
+
+    console.log("👀 Roles that exist right now:", allRoles);
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await token.roles.setAll({ admin: [], minter: [] });
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await token.roles.getAll()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
